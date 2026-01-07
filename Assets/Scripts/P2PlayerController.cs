cd83c61c32dd12c6235d77e80d4a9040352ecb06
Anta <j0siddy@gmail.com>
2026-01-07 11:23:49 -0600

    Starting prefab creation work

--- Assets/Scripts/P2PlayerController.cs
+++ Assets/Scripts/P2PlayerController.cs
@@ -5,6 +5,7 @@ public class P2PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
+    public GameObject projectilePrefab;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -24,6 +25,11 @@ public class P2PlayerController : MonoBehaviour
         if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
 
         horizontalInput = Input.GetAxis("Horizontal");
